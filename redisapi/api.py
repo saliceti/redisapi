f6e23927571a3c25755bdb1cd6a7da95148c970e
Colin Saliceti <c.saliceti@gmail.com>
2015-11-12 12:04:36 +0000

    space

--- redisapi/api.py
+++ redisapi/api.py
@@ -1,7 +1,7 @@
 # Copyright 2015 redisapi authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
-
+#
 import json
 import os
 
